320709703c4671927906cdfaa19912d90b5ed908
Felipe Geroldi <42786299+felipegeroldi@users.noreply.github.com>
2022-05-04 11:07:34 -0300

    Crud Dapper + Dapper.Contrib

--- Program.cs
+++ Program.cs
@@ -10,7 +10,10 @@ public class Program
 
     static void Main(string[] args)
     {
-
+        // ReadUsers();
+        // ReadUser();
+        // CreateUser();
+        DeleteUser();
     }
 
     public static void ReadUsers()
@@ -23,6 +26,66 @@ public class Program
             {
                 Console.WriteLine(user.Name);
             }
+        }
+    }
+
+    public static void ReadUser()
+    {
+        using (var connection = new SqlConnection(CONNECTION_STRING))
+        {
+            var user = connection.Get<User>(1);
+
+            Console.WriteLine(user.Name);
+        }
+    }
+
+    public static void CreateUser()
+    {
+        var user = new User()
+        {
+            Bio = "Teste",
+            Email = "teste@teste.com",
+            Image = "https://...",
+            Name = "Usuario Teste",
+            PasswordHash = "HASH",
+            Slug = "usuario-teste"
+        };
+
+        using (var connection = new SqlConnection(CONNECTION_STRING))
+        {
+            connection.Insert<User>(user);
+            Console.WriteLine("Cadastro realizado com sucesso");
+        }
+    }
+
+    public static void UpdateUser()
+    {
+        var user = new User()
+        {
+            Id = 2,
+            Bio = "Teste de Usuário",
+            Email = "teste@teste.com.br",
+            Image = "https://...",
+            Name = "Usuario de Teste",
+            PasswordHash = "HASH",
+            Slug = "usuario-teste"
+        };
+
+        using (var connection = new SqlConnection(CONNECTION_STRING))
+        {
+            connection.Update<User>(user);
+            Console.WriteLine("Atualização de usuário realizada com sucesso");
+        }
+    }
+
+    public static void DeleteUser()
+    {
+        using (var connection = new SqlConnection(CONNECTION_STRING))
+        {
+            var user = connection.Get<User>(2);
+
+            connection.Delete<User>(user);
+            Console.WriteLine("Remoção de usuário realizada com sucesso");
         }
     }
 }
